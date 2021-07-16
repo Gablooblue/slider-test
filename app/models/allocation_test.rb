76ad90b7c73da65888b4ddde49c0454f8c5831ac
Gabriel Lopez <gabrielhenry.lopez@gmail.com>
2021-07-16 17:33:33 +0800

    cahgne lottery

--- app/models/allocation_test.rb
+++ app/models/allocation_test.rb
@@ -13,11 +13,14 @@ class AllocationTest < ApplicationRecord
   def check_lottery
     y = 0
     correct_sliders = self.user.current_slide_score
+    puts correct_sliders
     allocation_items.each do |j|
-      y += (j.points - (j.get_range.include?(correct_sliders) ? 100 : 0)) ** 2
+      parsed = j.name.split("-")
+      #y += (j.points - (j.get_range.include?(correct_sliders) ? 100 : 0)) ** 2
+      y += (correct_sliders.to_i.between?(parsed[0].to_i, parsed[1].to_i) ? j.points : 0)
     end
 
-    if y < rand(20000) 
+    if y >= rand(100) 
       self.lottery_won = true
     else
       self.lottery_won = false
